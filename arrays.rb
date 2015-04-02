e2fb40b7dd6aaaa70c4108fff676aa2f1115dc20
satish.knwr <satish.knwr@gmail.com>
2015-04-02 17:05:55 +0545

    Added lots of comments

--- arrays.rb
+++ arrays.rb
@@ -1,9 +1,31 @@
 vaules = Array[]
 for i in 1..5 do
-	puts "Enter a number"
-	a=gets.chomp
-	vaules.push(a)
+puts "Enter a number"
+a=gets.chomp
+vaules.push(a)
 end
 for i in 0..4 do
-	puts vaules.sort[i]
+puts vaules.sort[i]
 end
+# vaules = Array[]
+# # puts "Enter how manu numbers to be sorted"
+# # a = gets.chomp
+# # a = a.to_i
+# # a.times.each do
+# for i in 1..5 do
+# 	puts "Enter a number"
+#   	b=gets.chomp
+#  	vaules.push(i)
+#  # end
+# # for i in 0..4 do
+# # a.times.each do
+
+# # 	puts vaules.sort[b]
+# #  end
+
+# # puts "Enter a number"
+# # a  = gets.chomp
+# # a  = a.to_i
+# # a.times.each do 
+# #   puts "Hello Satish"
+# # end
