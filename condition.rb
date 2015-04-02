8b7a23afd4a7a066ddecc71f005598f4c8ae799a
satish.knwr <satish.knwr@gmail.com>
2015-04-02 13:39:03 +0545

    Condition resolved

--- condition.rb
+++ condition.rb
@@ -1,12 +1,12 @@
 puts "Enter number"
-b = gets
+b = gets.chomp
 puts "Enter another number"
-c = gets
+c = gets.chomp
 d=Integer(b) + Integer(c)
 e=Integer(b) - Integer(c)
 puts "Press 1 for addition and 2 for subtraction"
-a=gets
-if a==1
+a=gets.chomp
+if a == 1.to_s
 	puts "Result of addition"
 	puts d
 else
